f62623ac1127029254d95a1390937170ccd4e796
sa1383 <saeed.samimirashkhar@partsoftware.com>
2025-06-30 13:43:49 +0330

    test: add some invalid codes to test eslint's reviewdog functionality (#15)
    * ADD some invalid syntaxes
    
    * FIX: eslint invalids

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello World');
+const fs = require('fs');
+const path = require('path');
+
+const fileName = fs.readFile(path.join(__dirname + './package.json'));
+console.log('filename: ', fileName);
